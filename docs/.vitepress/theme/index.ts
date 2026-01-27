92826616bced2ac989e894e7c6fb52609ca87ec1
FURYMOB <epfm07@gmail.com>
2026-01-27 15:23:59 +0100

    Hide search bar on home page via body class injection

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,22 @@
 import DefaultTheme from 'vitepress/theme'
+import { watch } from 'vue'
+import { useRoute } from 'vitepress'
 import './style.css'
 
 export default {
-    extends: DefaultTheme
+    extends: DefaultTheme,
+    setup() {
+        const route = useRoute()
+        watch(
+            () => route.path,
+            () => {
+                if (route.data.frontmatter.layout === 'home') {
+                    document.body.classList.add('home-page')
+                } else {
+                    document.body.classList.remove('home-page')
+                }
+            },
+            { immediate: true }
+        )
+    }
 }
